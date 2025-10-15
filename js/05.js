2f4be0406fc765a73c3fa3d6d8f23101e434c254
JPadillaTirado <jorluis7796@hotmail.com>
2025-10-14 21:20:20 -0500

    :sparkles: Update 05.js to include object sealing, freezing examples, and property modification

--- js/05.js
+++ js/05.js
@@ -16,6 +16,13 @@ console.table(persona);// Muestra el objeto en formato tabla
 persona.ubicacion = 'Nueva York'; // Agregar una nueva propiedad
 console.log(persona);
 console.table(persona);
+//Object.freeze(persona); // Congelar el objeto para evitar modificaciones
+Object.seal(persona); // Sellar el objeto para evitar agregar o eliminar propiedades, pero permite modificar las existentes
+persona.edad = 46; // Modificar una propiedad existente
 delete persona.edad; // Eliminar una propiedad
 console.log(persona);
 console.table(persona);
+console.log(`Hola, soy ${persona.nombre} ${persona.apellido} y tengo ${persona.edad} años. además soy ${persona.profesion}`);
+//como puedo evitar que a un objeto se le agreguen nuevas propiedades?
+Object.freeze(persona); // Congelar el objeto para evitar modificaciones
+persona.nacionalidad = 'Estadounidense'; // Intentar agregar una nueva propiedad (no tendrá efecto)
